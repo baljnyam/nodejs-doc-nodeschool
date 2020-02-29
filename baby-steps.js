5909874a90c527bea2a8b821bf2cbe135be486a6
baljnyam <baljnyam@gmail.com>
2020-03-01 09:32:08 +1100

    .

--- baby-steps.js
+++ baby-steps.js
@@ -1,1 +1,8 @@
-console.log( Math.sum(process.argv[2]) );
+sum = 0;
+process.argv.forEach((val, index) => {
+  if (Number(index) in [1, 0]) {
+    return;
+  }
+  sum = sum + Number(val);
+});
+console.log(sum);
